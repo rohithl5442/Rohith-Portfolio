1427c051749e7bc132e93a6f734ca9efae62f466
Rohith Suresh <rohithl5442@gmail.com>
2024-10-05 23:36:54 -0400

    Second commit - Involves the Setup, Navbar & Hero section.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,7 +1,14 @@
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { Hero } from "./components/Navbar/Hero/Hero";
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
-  return <div className={styles.App}>Hello World!</div>
+  return (
+  <div className={styles.App}>
+    <Navbar />
+    <Hero />
+  </div>
+  );
 }
 
 export default App
