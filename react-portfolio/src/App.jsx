d834eddd9d42e87f18d3d18c181ef982d2259d11
Rohith Suresh <rohithl5442@gmail.com>
2024-10-06 00:54:13 -0400

    Third commit - Hero section & About section completion

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
-import { Hero } from "./components/Navbar/Hero/Hero";
+import { About } from './components/About/About';
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   <div className={styles.App}>
     <Navbar />
     <Hero />
+    <About />
   </div>
   );
 }
