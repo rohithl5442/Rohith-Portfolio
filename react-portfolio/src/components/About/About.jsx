618685dbf978a2399dc4d3d27aef8f72e2f1b8ef
Rohith Suresh <rohithl5442@gmail.com>
2024-10-07 21:52:48 -0400

    Updated Hero and About jsx

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
+import React from "react";
 
 import styles from "./About.module.css";
-import { getImageUrl } from '../../utils';
+import { getImageUrl } from "../../utils";
 
 export const About = () => {
-  return ( 
-  <section className={styles.container} id="about">
-    <h2 className={styles.title}>About</h2>
-    <div className={styles.content}>
-        <img 
-        src={getImageUrl("about/aboutImage.png")} 
-        alt="Me sitting with a laptop" 
-        className={styles.aboutImage}
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
-            <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-                <div className={styles.aboutItemText}>
-                    <h3>Frontend Developer</h3>
-                    <p>
-                        I'm a frontend developer with experience in building responsive 
-                        and optimized sites
-                    </p>
-                </div>
-            </li>
-            <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-                <div className={styles.aboutItemText}>
-                    <h3>Backend Developer</h3>
-                    <p>
-                        I have experience developing fast and optimized backend systems and API's. 
-                    </p>
-                </div>
-            </li>
-            <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
-                <div className={styles.aboutItemText}>
-                    <h3>UI Designer</h3>
-                    <p>
-                        I have designed multiple landing pages and have created design systems as well.
-                    </p>
-                </div>
-            </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <br />
+              <p>
+              I'm a Frontend Developer with extensive experience in building responsive, optimized, and user-friendly websites. 
+              My expertise lies in creating dynamic and visually appealing UIs that enhance user experiences using technologies 
+              like React, JavaScript, HTML, and CSS. I’m proficient in ensuring cross-browser compatibility and mobile responsiveness, 
+              making sure the design scales across various devices.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <br />
+              <p>
+              I have a strong background in backend development, with a focus on building efficient, secure, and scalable systems. 
+              I specialize in Node.js, Python, SQL, and API development to ensure robust server-side architecture. 
+              My backend expertise involves database management, performance optimization, and API integration to handle complex 
+              data operations and interactions seamlessly.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/UX Designer</h3>
+              <br />
+              <p>
+              As a UI/UX designer, I’ve designed multiple user-centric landing pages and comprehensive design systems. 
+              My designs prioritize usability, clarity, and visual coherence, ensuring that users have an intuitive and enjoyable experience. 
+              With experience in Figma, Adobe XD, and other design tools, I create modern interfaces that align with the latest design trends.
+              </p>
+            </div>
+          </li>
         </ul>
-    </div>
-  </section>
+      </div>
+    </section>
   );
 };
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,10 +7,25 @@ export const Hero = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Rohith</h1>
+        <h1 className={styles.title}>Hi, I'm Rohith Suresh</h1>
         <p className={styles.description}>
-            I'm a Web Developer with 2 years of experience using React and 
-            NodeJS. Reach out if you'd like to learn more about me!
+        Welcome to my personal portfolio! 
+        <br />
+        <br />
+        I am an Aspiring Data Scientist and Full-Stack Developer with hands-on experience in designing and building scalable, 
+        secure web applications and software solutions. With a solid foundation in Data Science, Software Development, and Project Management, 
+        I have honed my skills in Python, SQL, Java, C, C++, and full-stack development using React, Node.js, and JavaScript.
+        <br />
+        <br />
+        I have successfully led multiple projects, ranging from front-end web development to back-end API management, with notable contributions to 
+        the U.S. Department of Defense and other key initiatives. These experiences have equipped me with the technical expertise to build robust, 
+        secure systems and deliver data-driven solutions.
+        <br />
+        <br />
+        I am passionate about applying Data Science methodologies to solve real-world challenges, and I thrive on using data to drive meaningful insights 
+        and impactful decisions. Currently, I am in my senior year, graduating with a Bachelor of Science in Computer Science from Wayne State University 
+        this December 2024. I am actively seeking opportunities to leverage my skills in Data Science and Software Development to contribute to impactful projects.
+
             </p>
             <a href="mailto: hl5442@wayne.edu" className={styles.contactBtn}>Contact Me</a>
         </div>
