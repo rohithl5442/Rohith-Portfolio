05e27be8264ccae23eea5b995d19b6b94e05f82a
Rohith Suresh <rohithl5442@gmail.com>
2024-10-08 00:52:44 -0400

    Headings size fix

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        PORTFOLIO
       </a>
       <div className={styles.menu}>
         <img
